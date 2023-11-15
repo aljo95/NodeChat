a326ccd2377ee2676b1a7b1cdcf0bf1851cca869
aljo <johansson_a95@hotmail.com>
2023-11-15 22:29:27 +0100

    Login form

---
+++ client/src/Components/Login.js
@@ -0,0 +1,89 @@
+import './App.css';
+//import axios from 'axios';
+import React, { useState } from 'react';
+
+export default function Login() {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleuserChange = (e) => {
+    setUsername(e.target.value);
+  }
+  const handlepwChange = (e) => {
+    setPassword(e.target.value);
+  }
+
+
+/*
+  const handleForm = async (e) => {
+    
+    console.log(username);
+    e.preventDefault();
+    
+    const userData = {
+      username: username,
+      password: password,
+    };
+
+    try {
+      const add = await fetch("/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+      console.log(add);
+    } catch(err) {
+      console.error();
+    }
+  };
+*/
+const handleForm = async (e) => {
+    
+    console.log(username);
+    e.preventDefault();
+    
+    const userData = {
+      username: username,
+      password: password,
+    };
+
+    fetch('http://localhost:8080/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+      }).then((res) => console.log(res));
+    
+  };
+
+  return (
+    <div className="App">
+      <h2 className='center'>Login</h2>
+        <form className='center' onSubmit={handleForm}>
+          <div>
+            <label>Username: </label>
+            <input type='text' name='username' value={username} onChange={handleuserChange}></input>
+          </div>
+          {
+          <div>
+            <label>Password: </label>
+            <input type='password' name='password' value={password} onChange={handlepwChange}></input>
+          </div>
+        }
+          <div>
+            <input type='submit' value='Log in'></input>
+          </div>
+        </form>
+
+
+
+      
+    </div>
+  );
+}
+/*
+export default Login;*/
