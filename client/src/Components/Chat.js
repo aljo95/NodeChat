e8bddcaa9cef0dcc9f8b687191e15ee7d11caa94
aljo <johansson_a95@hotmail.com>
2023-12-09 22:21:11 +0100

    emit messages with socket io

--- client/src/Components/Chat.js
+++ client/src/Components/Chat.js
@@ -4,13 +4,22 @@ import { useNavigate, Link } from "react-router-dom";
 import socketIO from 'socket.io-client';
 
   //const socket = socketIO.connect('http://127.0.0.1:8080');
+  const socket = socketIO.connect('http://127.0.0.1:8080');
 
 export default function Chat() {
 
     const [username, setUsername] = useState("");
     const navigate = useNavigate();
 
+    const [messages, setMessages] = useState([]);
+    const [messageText, setMessageText] = useState('');
+
     useEffect(() => {
+
+      /////////////////////////////////
+      socket.on('store-user');
+//////////////////////////////
+
         fetch('http://127.0.0.1:8080/api/checkAuth', {
           credentials: "include",
         })
@@ -23,12 +32,39 @@ export default function Chat() {
                 //redirect back to home (root)
                 navigate("/");
             } else {
-              const socket = socketIO.connect('http://127.0.0.1:8080');
+              
+
+              socket.on('connect', () => {
+                console.log('Connected to server');
+              });
+          
+              socket.on('disconnect', () => {
+                console.log('Disconnected from server');
+              });
+
             }
 
           })
         })
     }, [])
+
+
+
+    useEffect(() => {
+      socket.on('message', (message) => {
+        setMessages([ ...messages, message])
+      })
+    }, [messages])
+
+
+    const sendMessage = () => {
+      socket.emit('sendMessage', { text: messageText });
+      setMessageText('');
+    }
+
+
+
+
 
     const logout = (e) => {
         //DESTROY SESSION AND SET STATE TO FALSE
@@ -43,6 +79,36 @@ export default function Chat() {
 
   return (
     <div>
+
+
+      <div>
+        <h1>CHAT HERE</h1>
+        <div className="messages">
+          {messages.map((message, index) => (
+            <div className="nameAndMessage-container" key={index}>
+              <p className="message-username">{username}</p>
+              <p className="message-text">{message.text}</p>
+            </div>
+          ))}
+        </div>
+        <div className="input-container">
+          <input type="text" value={messageText} onChange={(e) => setMessageText(e.target.value)} placeholder="Type message here..."></input>
+          <button onClick={sendMessage}>SEND MSG</button>
+        </div>
+
+
+      </div>
+
+
+
+
+
+
+
+
+
+
+
         <div id="profile-menu">     {/* These css ids are not created yet */}
             <Link to='/profile'>
                 <button id="profile" className='btns'>
