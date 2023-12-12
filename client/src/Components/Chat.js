298df88987f9bceca87b270608c7880d8e67f8b7
aljo <johansson_a95@hotmail.com>
2023-12-12 23:26:29 +0100

    containerized better

--- client/src/Components/Chat.js
+++ client/src/Components/Chat.js
@@ -3,9 +3,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import { useNavigate, Link } from "react-router-dom";
 import socketIO from 'socket.io-client';
 
-  //const socket = socketIO.connect('http://127.0.0.1:8080');
   const socket = socketIO.connect('http://127.0.0.1:8080');
-  let data = {};
 
 export default function Chat() {
 
@@ -15,14 +13,9 @@ export default function Chat() {
     const [messages, setMessages] = useState([]);
     const [messageText, setMessageText] = useState('');
 
-    let nickname = "";
-    //let data = {hehe: "hoho"};
-
+    
+    
     useEffect(() => {
-
-      /////////////////////////////////
-      
-//////////////////////////////
 
         fetch('http://127.0.0.1:8080/api/checkAuth', {
           credentials: "include",
@@ -34,10 +27,7 @@ export default function Chat() {
                 //redirect back to home (root)
                 navigate("/");
             } else {
-              nickname = jsonResponse.username;
-              
-              data = {name: nickname, userId: socket.id};
-              console.log(data);
+              setUsername(jsonResponse.username);
 
               socket.on('connect', () => {
                 console.log('Connected to server');
@@ -63,11 +53,9 @@ export default function Chat() {
 
 
     const sendMessage = () => {
-      socket.emit('setSockedId', data);
-      console.log("-----------");
-      console.log(data);
-      console.log("-----------");
-      socket.emit('sendMessage', { text: messageText });
+
+      //socket.emit('sendMessage', { name: nickname, text: messageText });
+      socket.emit('sendMessage', { name: username, text: messageText });
       setMessageText('');
     }
 
@@ -83,28 +71,45 @@ export default function Chat() {
 
 
   return (
-    <div>
+    <div className='chat-room'>
+
+      <div className="nav-bar">
+        <Link to='/profile'>
+          <button id="profile" className='btns'>
+            <p>PROFILE</p>
+          </button>
+        </Link>
+        <button id="login" className='btns' onClick={logout}>
+          <p>LOGOUT</p>
+        </button>
+      </div>
 
 
-      <div>
-        <h1>CHAT HERE</h1>
+      <div className="main-content">
+        <div className="users-list">
+          <p id="users-online">USERS ONLINE:</p>
+
+        </div>
+
+
+
         <div className="messages">
-          {messages.map((message, index) => (
-            <div className="nameAndMessage-container" key={index}>
-              <p className="message-username">{data.userId}</p>
-             {/*  <p className="message-text">{message.text}</p> */}
-            </div>
-          ))}
+            {messages.map((message, index) => (
+              <div className="nameAndMessage-container" key={index}>
+                <p className="message-username">{message.name}</p>
+                <p className="message-text">{message.text}</p>
+              </div>
+            ))}
         </div>
-        <div className="input-container">
-          <input type="text" value={messageText} onChange={(e) => setMessageText(e.target.value)} placeholder="Type message here..."></input>
-          <button onClick={sendMessage}>SEND MSG</button>
-        </div>
-
+        
 
       </div>
 
 
+      <div className="input-container">
+            <input type="text" value={messageText} onChange={(e) => setMessageText(e.target.value)} placeholder="Type message here..."></input>
+            <button onClick={sendMessage}>SEND MSG</button>
+          </div>
 
 
 
@@ -112,26 +117,6 @@ export default function Chat() {
 
 
 
-
-
-        <div id="profile-menu">     {/* These css ids are not created yet */}
-            <Link to='/profile'>
-                <button id="profile" className='btns'>
-                    <p>PROFILE</p>
-                </button>
-            </Link>
-        </div>
-
-
-      <p>Username: {username}</p>
-      <Link to='/chat'>
-        <button id="register" className='btns'>
-          <p>CHAT</p>
-        </button>
-      </Link>
-      <button id="login" className='btns' onClick={logout}>
-        <p>LOGOUT</p>
-      </button>
     </div>
   );
 }
