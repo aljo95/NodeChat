c5fa514aa6b839f3a36aaa9a17d73b6baef98372
aljo <johansson_a95@hotmail.com>
2023-12-14 22:03:13 +0100

    paths to components with react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,34 @@
 
 
 import Home from './Components/Home';
-import  Login  from './Components/Login';
-import  Register  from './Components/Register';
+import Login from './Components/Login';
+import Register from './Components/Register';
+import Profile from './Components/Profile';
+import Chat from './Components/Chat'
 import './App.css';
-import React /*,{ Component }*/ from 'react';
+import React ,{ useState, createContext } from 'react';
 import { Route, Routes } from 'react-router-dom';
 //import ReactDOM from 'react-dom/client';
 
+export const UserContext = React.createContext(null);
+
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <main>
+      <UserContext.Provider value={{ isLoggedIn: isLoggedIn, setIsLoggedIn: setIsLoggedIn }}>
       <Routes>
-        <Route path='/' Component={Home} exact />
-        <Route path="/login" Component={Login} />
-        <Route path='/register' Component={Register}/>
+        
+          <Route path='/' Component={Home} exact />
+          <Route path="/login" Component={Login} />
+          <Route path='/register' Component={Register}/>
+          <Route path='/profile' Component={Profile} />
+          <Route path='/chat' Component={Chat} />
+        
       </Routes>
+      </UserContext.Provider>
     </main>
   );
 }
