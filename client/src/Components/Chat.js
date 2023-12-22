1b1fced0d5a6f97f7045452ceec44b2ba8612f96
aljo <johansson_a95@hotmail.com>
2023-12-22 22:45:01 +0100

    fix users list

--- client/src/Components/Chat.js
+++ client/src/Components/Chat.js
@@ -1,20 +1,27 @@
 import './App.css';
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useRef, useReducer } from 'react';
 import { useNavigate, Link } from "react-router-dom";
-import socketIO from 'socket.io-client';
+import  socketIO  from 'socket.io-client';
 
-  const socket = socketIO.connect('http://127.0.0.1:8080');
+
+var socket = socketIO.connect('http://127.0.0.1:8080');
 
 export default function Chat() {
 
     const [username, setUsername] = useState("");
-    const navigate = useNavigate();
+    const [usernames, setUsernames] = useState([]);
 
     const [messages, setMessages] = useState([]);
     const [messageText, setMessageText] = useState('');
 
-    
-    
+    const [sameUserAsLastMessage, setSameUserAsLastMessage] = useState(false);
+
+    const [ignored, forceUpdate] = useReducer(x => x + 1, 0);
+    const navigate = useNavigate();
+    const ref = useRef();
+    const date = new Date();
+    const weekday = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+
     useEffect(() => {
 
         fetch('http://127.0.0.1:8080/api/checkAuth', {
@@ -26,49 +33,101 @@ export default function Chat() {
             if (!jsonResponse.username) {
                 //redirect back to home (root)
                 navigate("/");
+                //break needed?
             } else {
               setUsername(jsonResponse.username);
+              
 
               socket.on('connect', () => {
                 console.log('Connected to server');
               });
-          
+
+              socket.emit('sendUsername', (jsonResponse.username));
+
+          /*
               socket.on('disconnect', () => {
                 console.log('Disconnected from server');
               });
-
+          */
             }
-
+            return ()=>{ 
+              socket.disconnect(); 
+             }
           })
-        })
+        });
     }, [])
 
-
+    useEffect(() => {
+      ref.current.scrollIntoView();
+    })
 
     useEffect(() => {
       socket.on('message', (message) => {
-        setMessages([ ...messages, message])
+        //message has { message.name, message.time, message.text } as object
+
+        if (messages.length === 0) {
+          setMessages([message]);
+        }
+
+        for (let i=messages.length-1; i>=0; i--) {
+          if (messages[i].name) {
+            console.log(messages[i]);
+            if (messages[i].name !== message.name) {
+              setMessages([ ...messages, message]);
+              return;
+            } else if (messages[i].name === message.name) {
+              setMessages([ ...messages, message.text]);
+              return;
+            }
+          }
+        }
       })
     }, [messages])
 
+    useEffect(() => {
+      
+      socket.on('username', (userNames) => {
+        
+        setUsernames(userNames);
+        forceUpdate();          //needed?
+      })
+
+    }, [usernames])
+
+    // dont need later
+    useEffect(() => {
+      console.log("amount of users: " + usernames.length);
+    }, [usernames])
+
 
     const sendMessage = () => {
-
-      //socket.emit('sendMessage', { name: nickname, text: messageText });
+      let currentDay = weekday[date.getDay()];
+      let currentTime = (date.toLocaleString()).slice(11, 16);
+      let fullTimeDisplay = currentDay + " " + currentTime;
+      //socket.emit('sendMessage', { name: username, time: fullTimeDisplay, text: messageText });
       socket.emit('sendMessage', { name: username, text: messageText });
       setMessageText('');
     }
 
-    const logout = (e) => {
-        //DESTROY SESSION AND SET STATE TO FALSE
-        
-        //THEN REDIRECT TO HOME (AKA ROOT "/")
+    const logout = () => {
+
+      fetch('http://127.0.0.1:8080/api/logout', {
+        credentials: "include",
+      })
+      .then((res) => {
+        return res.json().then((jsonRes) => {
+          console.log(jsonRes);
+          
+          socket.emit('removeUser', username);
+          socket.on('updatedUsers', async (updatedUserArray) => {
+            await setUsernames(updatedUserArray);        
+          })
+          
+
+          navigate("/");
+        })
+      })
     }
-
-
-
-
-
 
   return (
     <div className='chat-room'>
@@ -84,39 +143,43 @@ export default function Chat() {
         </button>
       </div>
 
-
       <div className="main-content">
         <div className="users-list">
-          <p id="users-online">USERS ONLINE:</p>
+          <p id="users-online">USERS</p>
+          {usernames.map((nick, index) => (
+            <div key={index} className="each-user-container">
 
+              <div className="green-orb"></div>
+              <p className="username-list">{nick}</p>
+
+            </div>
+          ))}
         </div>
-
-
 
         <div className="messages">
-            {messages.map((message, index) => (
-              <div className="nameAndMessage-container" key={index}>
-                <p className="message-username">{message.name}</p>
-                <p className="message-text">{message.text}</p>
+          {messages.map((message, index) => ( //need conditionals here: if (message.text) else... etc.
+          <div className="nameAndMessage-container" key={index}>
+          {message.text ? 
+            <>
+              <div className="name-and-time">
+                <p className="message-username"><b>{message.name}</b>&nbsp;</p>
+                <p className="message-time">{message.time}</p>
               </div>
-            ))}
+               <p className="message-text">{message.text}</p>
+            </>  
+            :
+            <p className="message-text">{message}</p>
+          }  
+          </div>
+          ))};
+          <div ref={ref}></div>
         </div>
-        
-
       </div>
 
-
       <div className="input-container">
-            <input type="text" value={messageText} onChange={(e) => setMessageText(e.target.value)} placeholder="Type message here..."></input>
-            <button onClick={sendMessage}>SEND MSG</button>
-          </div>
-
-
-
-
-
-
-
+        <input type="text" value={messageText} onChange={(e) => setMessageText(e.target.value)} placeholder="Type message here..."></input>
+        <button onClick={sendMessage}>SEND MSG</button>
+      </div>  
     </div>
   );
 }
