dc1a25b4986686dfab6438768d0d19193e7667bd
aljo <johansson_a95@hotmail.com>
2024-01-10 22:58:01 +0100

    simplify navigational buttons

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,30 +1,47 @@
 import './App.css';
 import React  from 'react';
-import { Link } from 'react-router-dom';
-//import  Login  from './Components/Login';
-//import  Register  from './Components/TestRoute';
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
 
-
+  const navigate = useNavigate();
 
   return (
     <div className="home">
       <h2 id='welcome'><p>NodeChat</p></h2>
+      <div id="home-links">
+          <button id="login" onClick={() => navigate("/login")}>
+            <p>LOGIN</p>
+          </button>
 
-      <Link to='/login'>
-        <button id="login" className='btns'>
-          <p>LOGIN</p>
-        </button>
-      </Link>
-
-      <Link to='/register'>
-        <button id="register" className='btns'>
-          <p>REGISTER</p>
-        </button>
-      </Link>
-
+          <button id="register" onClick={() => navigate("/register")}>
+            <p>REGISTER</p>
+          </button>
+      </div>
       
     </div>
   );
 }
+
+/*
+  return (
+    <div className="home">
+      <h2 id='welcome'><p>NodeChat</p></h2>
+      <div id="home-links">
+        <Link to='/login' className="links">
+          <button id="login">
+            <p>LOGIN</p>
+          </button>
+        </Link>
+
+        <Link to='/register' className="links" id="second-link">
+          <button id="register">
+            <p>REGISTER</p>
+          </button>
+        </Link>
+      </div>
+      
+    </div>
+  );
+}
+*/
