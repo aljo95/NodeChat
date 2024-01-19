d106977d55f10408bae363a76b31d20d14087e7d
aljo <johansson_a95@hotmail.com>
2024-01-19 20:45:11 +0100

    model for messages history

---
+++ server/messagesModel.js
@@ -0,0 +1,17 @@
+
+const mongoose = require("mongoose"); 
+  
+// Creating Message Model 
+const messageSchema = new mongoose.Schema({ 
+
+    //models must be string, string(required), date
+
+    name: { type: String },
+    text: { type: String, required: true }, 
+    time: { type: String },
+
+}); 
+  
+const Message = mongoose.model("Message", messageSchema); 
+  
+module.exports = Message; 
