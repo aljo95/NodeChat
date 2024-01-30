f74f099cf2b457adcb722fb9f6d439fbe6b612b8
aljo <johansson_a95@hotmail.com>
2024-01-30 22:47:04 +0100

    main app rendered ver 1.0

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,8 @@ import Register from './Components/Register';
 import Profile from './Components/Profile';
 import Chat from './Components/Chat'
 import './App.css';
-import React ,{ useState, createContext } from 'react';
+import React ,{ useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
-//import ReactDOM from 'react-dom/client';
 
 export const UserContext = React.createContext(null);
 
@@ -20,13 +19,11 @@ function App() {
     <main>
       <UserContext.Provider value={{ isLoggedIn: isLoggedIn, setIsLoggedIn: setIsLoggedIn }}>
       <Routes>
-        
           <Route path='/' Component={Home} exact />
           <Route path="/login" Component={Login} />
           <Route path='/register' Component={Register}/>
           <Route path='/profile' Component={Profile} />
           <Route path='/chat' Component={Chat} />
-        
       </Routes>
       </UserContext.Provider>
     </main>
