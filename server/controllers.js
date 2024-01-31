e29e6438847f71f440f0444a5006846d7bb1ca9c
aljo <johansson_a95@hotmail.com>
2024-01-31 02:03:41 +0100

    controllers finished ver 1.0

--- server/controllers.js
+++ server/controllers.js
@@ -1,4 +1,3 @@
-
 //controllers.js 
 const express = require("express"); 
 const router = express.Router(); 
@@ -7,27 +6,13 @@ const Messages = require("./messagesModel");
 const passport = require("passport"); 
 const bcrypt = require("bcrypt"); 
   
-
-
-///////////////////////////////// EXPERIMENTS
 const app = express();
 
 const bodyParser = require('body-parser');
   // support parsing of application/json type post data
-  app.use(bodyParser.json());
-  //support parsing of application/x-www-form-urlencoded post data
-  app.use(bodyParser.urlencoded({ extended: true }));
-// User registration route 
-
-/*
-router.post('/register', (req, res) => {
-    console.log("POSTEDEDED");
-})
-////////////////////////////////////
-*/
-
-
-
+app.use(bodyParser.json());
+  // support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
@@ -35,24 +20,16 @@ router.post('/register', async (req, res) => {
     console.log(req.body); 
     const { username, password} = req.body; 
     if (username==="" || password==="") { 
-        console.log("FILL BOTH!")
-        return res 
-            .status(403) 
-        //    .render("register", { error: "All Fields are required" }); 
+        console.log("FILL BOTH FIELDS!")
+        return res.status(403);
     } 
-
     try { 
         // Check if user already exists 
         const existingUser = await User.findOne({ username }); 
         if (existingUser) { 
-            console.log("ERROR USERNAME EXISTS");
-            
-            return res 
-                .status(409) 
-              //  .render("register", { error: "Username already exists" }); 
-            
+            console.log("ERROR USERNAME ALREADY EXISTS");
+            return res.status(409);
         } 
-  
         // Hash the password before saving it to the database 
         const salt = await bcrypt.genSalt(15); 
         const hashedPassword = await bcrypt.hash(password, salt); 
@@ -60,124 +37,105 @@ router.post('/register', async (req, res) => {
         // Create and save the new user 
         const newUser = new User({ username, password: hashedPassword }); 
         await newUser.save(); 
-        console.log("New user made xD");
+        console.log("New user made added!");
         return res.status(200).json({ message: "success" });
-        //return res.redirect("/login"); 
     } catch (err) { 
-        return res.status(500) //.json({ message: err.message }); 
+        return res.status(500); //.json({ message: err.message }); 
     } 
 }); 
   
 // User login route 
-router.post(                            // NEED TO FIND USER FROM DATABASE NOT JUST EXPECT IT TO EXIST LOL
-    '/login', 
-    passport.authenticate("local", { session: true }), //????????????????
+router.post('/login', 
+    passport.authenticate("local", { session: true }),
     async (req, res) => { 
-        
-        req.session.name = req.body.username; 
-        /*
-        console.log("XXXXXXXXXXXXXXXXXXXXXXX");
-        console.log(req.session);
-        console.log(req.session.name);
-        console.log("XXXXXXXXXXXXXXXXXXXXXXX");
-        req.session.save(); 
-        console.log("Logging in user: <" + req.body.username + "> ...");
-        return res.status(200).json({ message: "success" });
-        */
-
-       // await req.session.save();
-        console.log(req.session);
-        console.log("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
-        console.log(req.sessionID);
-        
-        console.log("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
-        //return 
+        req.session.name = req.body.username;
         res.status(200).json({ message: "success" });
-        console.log("THIS SHOULD NOT EXIST HERE!");
     }
 ); 
 
-
-
+// Route for saving history
 router.post('/storeMessage', async (req, res) => {
 
-    // req.body.message will hold either object or string. Just store like this in db
-    
-    // 1: req.body =  { message: { name: 'cunt', text: 'asdfasdf', time: 'Thu 15:06' } }
-    //  => req.body.message = { name: 'cunt', text: 'asdfasdf', time: 'Thu 15:06' }
+    // Will return 0 if there are no records!
+    let amountOfRecords = await Messages.countDocuments();
+    console.log("Amount of documents/records: " + amountOfRecords); 
 
-    // 2: req.body =  { message: 'asdfasdf' }   <-- object
-    //  => req.body.message = 'asdfasdf'        <-- string
-
-    
-    // BEFORE WE SAVE WE WANT TO ADD AN ID. IF NO RECORDS THEN ID=1, ELSE COUNT RECRODS!
-    /*
-    const user = await Messages.find(); 
-    console.log("JJJJJJJJJ")
-    console.log(user);
-    console.log("JJJJJJJJJ")
-*/
-
-console.log("JJJJJJJJJ")
-console.log(req.body);
-console.log("JJJJJJJJJ")
-
-    // Add a count variable to the body.message object to keep track of order
-    // First need to return the length of current collection:
-    const amountOfRecords = await Messages.countDocuments();
-    console.log("Amount of documents: " + amountOfRecords);
-
-    if (typeof req.body.message === "string") {
-        console.log("11111111111111111111111111111111111")
-        const newMsg = new Messages( { text: req.body.message } );
+    // Save 10 records (0-9), when reached that count delete 5 first, move 5-9 to 0-4, then add new
+    // Change numbers to higher values if real usage of chat app!
+    if (amountOfRecords >= 99) {
+        //Remove 0-4
+        console.log("Deleting records");
+        await Messages.deleteMany({ msgNr: {$lte: 49} });
+        //Update 5-9 with msgNr change to 0-4
+        for (let i=0; i<49; i++) {
+            await Messages.updateOne( { msgNr: i+50 }, { $set: { msgNr: i } } );
+        }
+        amountOfRecords = await Messages.countDocuments();
+    }
+    if (typeof req.body.message === "string") {     
+        const newMsg = new Messages( { text: req.body.message, msgNr: amountOfRecords } );
         await newMsg.save(); 
     }
     else if (typeof req.body.message === "object") {
-        console.log("22222222222222222222222222222222222")
+        req.body.message.msgNr = amountOfRecords;
         const newMsg = new Messages(req.body.message); 
         await newMsg.save(); 
     } 
-    
     else {
         console.log("Could not save history - incompatible data type with model");
         return res.status(500).json({ message: "could not save to mongoDB" });
     }
-
     return res.status(200).json({ message: "success" });
 });
 
-
+// Route to fetch history and display on front end
 router.get('/getHistory', async (req, res) => {
-    console.log("history gotteN^");
 
+    let historyResponseArray = [];
+    const fullHistory = await Messages.find();
 
-    res.send({ asfterhistory: "history success"});
+    //Sort by msgNr property to ensure correctness
+    fullHistory.sort((a, b) => a.msgNr - b.msgNr);
+
+    console.log(fullHistory);
+
+    for (let i=0; i<fullHistory.length; i++) {
+        if (fullHistory[i].name) {
+            let pushObj = { 
+                name: fullHistory[i].name, 
+                text: fullHistory[i].text,
+                time: fullHistory[i].time,
+            }
+            historyResponseArray.push(pushObj);
+        } else {
+            // Just pushing the strong for react state format
+            historyResponseArray.push(fullHistory[i].text);
+        }
+    }
+    for (let i=0; i<historyResponseArray.length; i++) {
+        if (historyResponseArray[i].name) {
+            historyResponseArray = historyResponseArray.slice(i);
+            break;
+        }
+    }
+    // Correct res format for just array? Include message or status code?
+    res.send(historyResponseArray);
 });
 
-
-
-
-
+// Checking if user is logged in or not
+// Used for debugging
 router.get('/checkAuth', (req, res) => {
 
-    
     if (req.isAuthenticated()) {
         console.log("LOGGED IN!");
     } else {
-        console.log("NOT LOGGED IN :<");
+        console.log("NOT LOGGED IN :(");
     }
-    console.log("-----------------------");
-    console.log(req.session);
-    console.log("-----------------------");
-    /*
-    console.log("----------------------");
-    console.log("in checkAuth: ");
-    console.log(req.session);
-    */
+    //console.log(req.session);
    res.send({ username: req.session.name } )
 })
 
-  
+// Route on user logout 
 router.get("/logout", (req, res) => { 
     req.session.destroy(); 
     res.status(200).json({ message: "logout success" }); 
