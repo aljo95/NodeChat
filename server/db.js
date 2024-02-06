f9ff1f075b6414e96bd0f6e66884f9d5861e6270
aljo <johansson_a95@hotmail.com>
2024-02-06 20:32:49 +0100

    fixing .env variable

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ dotenv.config({  path: './.env' });
 
 const connectDB = async() => {
  //Add db connect string in .env file with variable name MONGODB_CONNECT
-    mongoose.connect("process.env.MONGODB_CONNECT")
+    mongoose.connect(process.env.MONGODB_CONNECT)
     .then(() => {
         console.log("Connected to DB")
     })
